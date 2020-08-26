450dabdd32a6b1243896c3025963c73f5dae98f8
Hope Gochnour <hope.gochnour@gmail.com>
2020-08-26 09:24:40 -0600

    Add House above_market_price? method

--- test/house_test.rb
+++ test/house_test.rb
@@ -27,4 +27,18 @@ class HouseTest < Minitest::Test
 
     assert_equal [room_1, room_2], house.rooms
   end
+
+  def test_market_average
+    house = House.new("$400000", "123 sugar lane")
+
+    assert_equal false, house.above_market_average?
+  end
 end
+# room_1 = Room.new(:bedroom, 10, '13')
+# room_2 = Room.new(:bedroom, 11, '15')
+# room_3 = Room.new(:living_room, 25, '15')
+# room_4 = Room.new(:basement, 30, '41')
+# house.add_room(room_1)
+# house.add_room(room_2)
+# house.add_room(room_3)
+# house.add_room(room_4)
